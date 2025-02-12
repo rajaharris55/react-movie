dd6d001e84048d1dc68e15b239e62ab7862d9364
rajaharris55 <rajaharris55@gmail.com>
2025-02-12 18:13:24 +0500

    Api, display posters, search and hover effects

---
+++ frontend/movie/components/SearchBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBox = ({ searchValue, setSearchValue }) => {
+  return (
+    <div className="col col-sm-4 search-box">
+      <input
+        className="form-control"
+        placeholder="Enter a movie name..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      ></input>
+    </div>
+  );
+};
+
+export default SearchBox;
--- frontend/movie/components/homepage/MovieList.jsx
+++ frontend/movie/components/homepage/MovieList.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const MovieList = (props) => {
+  const FavComponent = props.favoritesComponent;
   return (
     <>
       {props.movies.map((movies, index) => (
-        <div>
-          <img src={movies.Poster} alt="movie" />
+        <div className="image-container d-flex justify-content-start m-3">
+          <div className="movie-list" key={index}>
+            <img src={movies.Poster} alt="movie" />
+          </div>
+          <div className="overlay d-flex justify-content-center align-items-center">
+            <FavComponent />
+          </div>
         </div>
       ))}
     </>
